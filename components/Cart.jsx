684a7a8d8c3577500c4b9ba15bbd0aa98f1d694b
LeoAdemi <Admin@LEO>
2022-10-10 14:25:43 +0200

    cart update

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -40,9 +40,53 @@ const Cart = () => {
             </Link>
           </div>
         )}
-        
-        </div>
+           <div className="product-container">
+            {cartItems.length >= 1 && cartItems.map((item,index) => (
+              <div className="product" key={item._id}>
+              <img src={urlFor(item?.image[0])} className="cart-product-image" />
+               <div className="item-desc">
+                <div className="flex top">
+                  <h5>{item.name}</h5>
+                  <h4>${item.price}</h4> 
+              </div>
+                <div className="flex bottom">
+                  <div> 
+                    <p className="quantity-desc">
+                    <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec') }>
+                    <AiOutlineMinus />
+                    </span>
+                    <span className="num" onClick="">{item.quantity}</span>
+                    <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }><AiOutlinePlus /></span>
+                   </p>
+                  </div>
+                  <button
+                    type="button"
+                    className="remove-item"
+                    onClick={() => onRemove(item)}
+                  >
+                    <TiDeleteOutline />
+                  </button>
+                  </div>
+            </div>
+          </div>
+           ))}
+           </div>
+           
+           {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick={handleCheckout}>
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
       </div>
+    </div>
   )
 }
 
