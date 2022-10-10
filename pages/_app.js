713a3bf62a36ce761f04c2db2fc5efc3b0b0e7a2
LeoAdemi <Admin@LEO>
2022-10-10 14:00:37 +0200

    cart,navbar and context

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
 import React from 'react';
-// import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
 import '../styles/globals.css';
-// import { StateContext } from '../context/StateContext';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <StateContext>
+    <StateContext>
       <Layout>
-        {/* <Toaster /> */}
+         <Toaster /> 
         <Component {...pageProps} />
       </Layout>
-    // </StateContext>
+    </StateContext>
   )
 }
 
